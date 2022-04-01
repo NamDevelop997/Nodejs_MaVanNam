b9006ab8a4a43fb150aaed3f764c23c5cd5bb694
namDevelop997 <namdevelop997@gmail.com>
2022-04-01 16:35:22 +0700

    B04-05. Exe filter by status

--- B04/config/system.js
+++ B04/config/system.js
@@ -1,4 +1,4 @@
 module.exports = {
-  prefix_admin: "admin",
+  prefix_admin: "admin123",
   full_name   : "Ma Van Nam"
 };
--- B04/routes/backend/item.js
+++ B04/routes/backend/item.js
@@ -2,17 +2,30 @@ var express = require("express");
 var router = express.Router();
 
 const ItemsModel = require("./../../schemas/items");
+const UtilsHelpers = require("./../../helpers/Utils");
+const paramsHelpers = require("./../../helpers/getParams");
 
 /* GET home page. */
 
-router.get("/", function (req, res, next) {
-  ItemsModel.find({}).then((items) => {
+//filter by status
+router.get("(/:status)?", function (req, res, next) {
+  let ObjWhere = {};
+  let currentStatus = paramsHelpers.getParams(req.params, 'status', 'active');
+  let filterStatus  = UtilsHelpers.filterStatus(currentStatus);
+  
+  if(currentStatus !== 'all') ObjWhere = {status : currentStatus};
+  
+
+  ItemsModel.find(ObjWhere).then((items) => {
     res.render("pages/backend/items/list", {
       pageTitle: "Items list page",
       items,
+      filterStatus
     });
   });
 });
+
+
 
 router.get("/login", function (req, res, next) {
   res.render("pages/frontend/login", { pageTitle: "Login", layout: false });
