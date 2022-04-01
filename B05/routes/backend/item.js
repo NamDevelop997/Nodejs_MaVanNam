344af7cd3517b30f89b0d09dd748e882c3f398a3
namDevelop997 <namdevelop997@gmail.com>
2022-04-01 23:59:00 +0700

    B05-02. Exe search

--- B05/routes/backend/item.js
+++ B05/routes/backend/item.js
@@ -7,32 +7,37 @@ const paramsHelpers = require("./../../helpers/getParams");
 
 /* GET home page. */
 
-//filter by status
-router.get("(/:status)?", function (req, res, next) {
-  let ObjWhere = {};
-  let currentStatus = paramsHelpers.getParams(req.params, 'status', 'active');
-  let filterStatus  = UtilsHelpers.filterStatus(currentStatus);
-  
-  if(currentStatus !== 'all') ObjWhere = {status : currentStatus};
-  
-
-  ItemsModel.find(ObjWhere).then((items) => {
-    res.render("pages/backend/items/list", {
-      pageTitle: "Items list page",
-      items,
-      filterStatus
-    });
-  });
+router.get("/form", function (req, res, next) {
+  res.render("pages/backend/items/form", { pageTitle: "Form items - Add" });
 });
-
-
 
 router.get("/login", function (req, res, next) {
   res.render("pages/frontend/login", { pageTitle: "Login", layout: false });
 });
 
-router.get("/form", function (req, res, next) {
-  res.render("pages/backend/items/form", { pageTitle: "Form items - Add" });
+//filter by status
+router.get("(/:status)?", function (req, res, next) {
+  let ObjWhere = {};
+  let currentStatus = paramsHelpers.getParams(req.params, 'status', 'all');
+  let keyword = paramsHelpers.getParams(req.query, 'keyword', "");
+  let filterStatus  = UtilsHelpers.filterStatus(currentStatus);
+  
+  if(currentStatus === 'all' && keyword !== '') {
+      ObjWhere = {name : { $regex: keyword, $options: "i" }};
+  }else {
+      ObjWhere = {status : currentStatus, name : { $regex: keyword, $options: "i" }};
+  }
+
+
+  ItemsModel.find(ObjWhere).then((items) => {
+    res.render("pages/backend/items/list", {
+      pageTitle: "Items list page",
+      items,
+      filterStatus,
+      currentStatus,
+      keyword
+    });
+  });
 });
 
 module.exports = router;
