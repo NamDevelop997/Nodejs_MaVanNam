09fb955870f8d139ed1da28fb012f2c906915341
namDevelop997 <namdevelop997@gmail.com>
2022-04-08 02:39:52 +0700

    B05-08. Exe panigation

--- B05/routes/backend/item.js
+++ B05/routes/backend/item.js
@@ -1,8 +1,8 @@
-var express = require("express");
-var router = express.Router();
+var express         = require("express");
+var router          = express.Router();
 
-const ItemsModel = require("./../../schemas/items");
-const UtilsHelpers = require("./../../helpers/Utils");
+const ItemsModel    = require("./../../schemas/items");
+const UtilsHelpers  = require("./../../helpers/Utils");
 const paramsHelpers = require("./../../helpers/getParams");
 
 /* GET home page. */
@@ -17,26 +17,46 @@ router.get("/login", function (req, res, next) {
 
 //filter by status
 router.get("(/:status)?", function (req, res, next) {
-  let ObjWhere = {};
-  let currentStatus = paramsHelpers.getParams(req.params, 'status', 'all');
-  let keyword = paramsHelpers.getParams(req.query, 'keyword', "");
-  let filterStatus  = UtilsHelpers.filterStatus(currentStatus);
-  
-  if(currentStatus === 'all'  ) {
-    if(keyword !== '') ObjWhere = {name : { $regex: keyword, $options: "i" }};
-  }else {
-      ObjWhere = {status : currentStatus, name : { $regex: keyword, $options: "i" }};
-  }
+  let ObjWhere        = {};
+  let currentStatus   = paramsHelpers.getParams(req.params, "status", "all");
+  let keyword         = paramsHelpers.getParams(req.query, "keyword", "");
+  let getPageOnURL    = paramsHelpers.getParams(req.query, "page", 1);
 
-  ItemsModel.find(ObjWhere).then((items) => {
-    res.render("pages/backend/items/list", {
-      pageTitle: "Items list page",
-      items,
-      filterStatus,
-      currentStatus,
-      keyword
+  let filterStatus    = UtilsHelpers.filterStatus(currentStatus);
+  let panigation      = {
+    totalItemsPerpage : 3,
+    currentPage       : getPageOnURL,
+    totalItems        : 1,
+    pageRanges        : 3
+  };
+
+  if (currentStatus === "all") {
+    if (keyword !== "") ObjWhere = { name: { $regex: keyword, $options: "i" } };
+  } else {
+    ObjWhere = {
+      status: currentStatus,
+      name: { $regex: keyword, $options: "i" },
+    };
+  }
+   ItemsModel.count(ObjWhere).then((data) => {
+    panigation.totalItems = data;
+    ItemsModel.find(ObjWhere)
+    .limit(panigation.totalItemsPerpage)
+    .skip((panigation.currentPage - 1) * panigation.totalItemsPerpage)
+    .sort({ ordering: "asc" })
+    .then((items) => {
+      res.render("pages/backend/items/list", {
+        pageTitle: "Items list page",
+        items,
+        filterStatus,
+        currentStatus,
+        keyword,
+        panigation,
+      });
     });
   });
+
+  
 });
 
 module.exports = router;
