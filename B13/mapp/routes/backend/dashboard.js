19515ccab319e933aedbbae32096ea63ea43d630
NamDevelop997 <namdevelop997@gmail.com>
2022-06-09 22:13:10 +0700

    B13. Init project News.

---
+++ B13/mapp/routes/backend/dashboard.js
@@ -0,0 +1,41 @@
+var express         = require('express');
+var router          = express.Router();
+
+const ItemsModel    = require("../../schemas/category");
+const GroupsModel   = require("../../schemas/groups");
+const UsersModel    = require("../../schemas/users");
+const ArticlesModel = require("../../schemas/articles");
+
+
+
+/* GET home page. */
+router.get('/', async(req, res, next) => {
+  
+  let countItems    = 0 ;
+  let countGroups   = 0 ;
+  let countUsers    = 0 ;
+  let countArticles = 0 ;
+  await ItemsModel.count({}).then((count) => { //get count Items
+    countItems = count;
+  });
+
+  await GroupsModel.count({}).then((count) => { //get count Groups
+    countGroups = count;
+  });
+  
+
+  await UsersModel.count({}).then((count) => { //get count Users
+    countUsers = count;
+  });
+
+  await ArticlesModel.count({}).then((count) => { //get count Users
+    countArticles = count;
+  });
+  res.render('pages/backend/dashboard/index', { pageTitle: 'Dashboard' , countItems, countGroups, countUsers, countArticles});
+});
+
+
+
+
+
+module.exports = router;
